738bd69a25bd0a748e2f5eb23d07fb0ff64cb958
odhyp <pradhana.odhy@gmail.com>
2024-01-15 10:34:29 +0700

    Add getters for input_path

--- src/utils.py
+++ src/utils.py
@@ -15,3 +15,11 @@ def get_data_path():
     root_path = get_root_path()
     data_path = Path(root_path, 'data')
     return data_path
+
+
+def get_input_path(file_name: str):
+    """Get input file path. Current input directory is in data
+    """
+    data_path = get_data_path()
+    input_path = Path(data_path, file_name)
+    return input_path
