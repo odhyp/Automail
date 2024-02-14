aac2fa553a63d729d5dbdb498b37bc45aa95cf24
odhyp <pradhana.odhy@gmail.com>
2024-02-14 21:19:20 +0700

    Add find_letter_number method

--- src/pdf.py
+++ src/pdf.py
@@ -14,3 +14,17 @@ class PDF:
             text = doc[0].get_text()
         trimmed_text = re.sub(r'^\s+|\s+$', '', text, flags=re.MULTILINE)
         return trimmed_text
+
+    def find_letter_number(self, file_path: str):
+        """Find the office letter number in the specified PDF
+
+        Args:
+        - file_path (str): The path to the PDF file
+        """
+        text = self.read_pdf(file_path=file_path)
+        pattern = r'Nomor\s*:\s*(\S.*?)\s+\b\w+\b'
+        match = re.search(pattern, text)
+        if match:
+            return match.group(1)
+        else:
+            return None
